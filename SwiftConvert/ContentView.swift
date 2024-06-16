011d2ddcf279e6046d333aa1bd50fdf5d7e40f2d
David Nwachukwu <davidnw1998@gmail.com>
2024-06-16 21:54:55 +0100

    ui and logic

--- SwiftConvert/ContentView.swift
+++ SwiftConvert/ContentView.swift
@@ -8,14 +8,89 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+	enum Unit: CaseIterable {
+		case milliliters, liters, cups, pints, gallons
+	}
+	
+	@State private var inputUnit = Unit.liters
+	@State private var outputUnit = Unit.milliliters
+	@State private var inputValue = 0.0
+	@FocusState private var inputIsFocused: Bool
+	
+	func getMultiplier(unit: Unit) -> Double {
+		switch unit {
+		case .liters:
+			return 1000
+		case .cups:
+			return 284.1307
+		case .pints:
+			return 568.2615
+		case .gallons:
+			return 4546.09
+		default:
+			return 1
+		}
+	}
+	
+	var result: Double {
+		if (inputUnit == outputUnit) {
+			return inputValue
+		}
+		
+		var inputInML = 0.0
+		
+		inputInML = inputValue * getMultiplier(unit: inputUnit)
+		
+		return inputInML / getMultiplier(unit: outputUnit)
+	}
+	
+	var body: some View {
+		NavigationStack {
+			Form {
+				Section {
+					TextField("Input", value: $inputValue, format: .number)
+						.keyboardType(.decimalPad)
+						.focused($inputIsFocused)
+				} header: {
+					Text("Input Value")
+				}
+				
+				Section {
+					Picker("From", selection: $inputUnit) {
+						ForEach(Unit.allCases, id: \.self) {
+							Text("\($0)")
+						}
+					}
+					.pickerStyle(.automatic)
+					
+					Picker("To", selection: $outputUnit) {
+						ForEach(Unit.allCases, id: \.self) {
+							Text("\($0)")
+						}
+					}
+					.pickerStyle(.automatic)
+				} header: {
+					Text("Select conversion units")
+				}
+				
+				Section {
+					Text(result, format: .number)
+				} header: {
+					Text("Result")
+				}
+			}
+			.navigationTitle("SwiftConvert")
+			.toolbar {
+				ToolbarItemGroup(placement: .keyboard) {
+					Spacer()
+					
+					Button("Done") {
+						inputIsFocused = false
+					}
+				}
+			}
+		}
+		.navigationViewStyle(.stack)
     }
 }
 
