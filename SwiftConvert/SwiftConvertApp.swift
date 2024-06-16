1e297d25ddbedb1d2c2bc5ab0d868256f6d3825e
David Nwachukwu <davidnw1998@gmail.com>
2024-06-16 19:05:07 +0100

    Initial Commit

---
+++ SwiftConvert/SwiftConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConvertApp.swift
+//  SwiftConvert
+//
+//  Created by David Nwachukwu on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
